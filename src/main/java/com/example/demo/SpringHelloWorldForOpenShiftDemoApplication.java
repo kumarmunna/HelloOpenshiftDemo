90c7bcb6fc73126d26679f03d34f8e97f6a796ca
Santosh Kumar Singh <educareer90@gmail.com>
2019-11-26 10:14:08 +0530

    Added first project

---
+++ src/main/java/com/example/demo/SpringHelloWorldForOpenShiftDemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringHelloWorldForOpenShiftDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloWorldForOpenShiftDemoApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String getHelloMessage() {
+		return "Hello World";
+	}
+	
+	
+	@GetMapping("/hello/{name}")
+	public String getHelloMessage(@PathVariable("name") String name) {
+		return "Welcome " + name +" on open shift deployment";
+	}
+
+}
